eafb8ee805283ce87de57f5fc05af8b4560aecb4
Ashini L J <156600873+Ashinilj@users.noreply.github.com>
2026-01-30 09:25:33 +0530

    Time: 5 ms (20.37%) | Memory: 61.8 MB (5.85%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let count=0;
+    let j=0;
+
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] != 0){
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            j++;
+        }
+    }
+    return nums;
+};
