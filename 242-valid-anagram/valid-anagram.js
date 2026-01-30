e8c8220c4742397223052b73b55c73ade0503e3e
Ashini L J <156600873+Ashinilj@users.noreply.github.com>
2026-01-30 10:19:08 +0530

    Time: 19 ms (33.63%) | Memory: 61.1 MB (10.87%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false;
+
+    let freq= new Map();
+    for(let ch of s){
+        freq.set(ch, (freq.get(ch) || 0) + 1);
+    }
+
+    for(let ch of t){
+        if(!freq.has(ch)){
+            return false;
+        }
+
+        freq.set(ch, freq.get(ch)-1);
+        if(freq.get(ch) < 0) return false;
+    }
+    return true;
+};
