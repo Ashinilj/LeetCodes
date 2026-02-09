a02a8ae7272a41bedff29f62688f4993556a6c92
Ashini L J <156600873+Ashinilj@users.noreply.github.com>
2026-02-09 23:27:43 +0530

    Time: 20 ms (29.07%) | Memory: 65.6 MB (77.49%) - LeetSync

---
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorderTraversal(TreeNode* root, vector<int>& nodes) {
+        if (!root) return;
+        inorderTraversal(root->left, nodes);
+        nodes.push_back(root->val);
+        inorderTraversal(root->right, nodes);
+    }
+
+    // Function to construct a balanced BST from sorted nodes
+    TreeNode* buildBalancedBST(vector<int>& nodes, int start, int end) {
+        if (start > end) return nullptr;
+        int mid = start + (end - start) / 2;
+        TreeNode* node = new TreeNode(nodes[mid]);
+        node->left = buildBalancedBST(nodes, start, mid - 1);
+        node->right = buildBalancedBST(nodes, mid + 1, end);
+        return node;
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int> nodes;
+        inorderTraversal(root, nodes); // Get nodes in sorted order
+        return buildBalancedBST(nodes, 0, nodes.size() - 1);
+    }
+};
