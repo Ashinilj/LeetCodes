c545ef8b6b6d7529e351e1fa60db346556ed3a8c
Ashini L J <156600873+Ashinilj@users.noreply.github.com>
2026-02-12 23:54:10 +0530

    Time: 1 ms (94.54%) | Memory: 62.2 MB (87.35%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let left= 0, right= height.length-1;
+    let left=0, right= height.length-1;
     let maxArea= Number.MIN_SAFE_INTEGER;
 
-    while(left< right){
+    while(left < right){
         let currArea= Math.min(height[left], height[right]) * (right-left);
-        maxArea= Math.max(currArea, maxArea);
+        maxArea= Math.max(maxArea, currArea);
 
-        if(height[left] > height[right]) right--;
-        else left++;
-
+        if(height[left] < height[right]) left++;
+        else right--;
     }
     return maxArea;
 };
