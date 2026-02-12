47d890782cdb5adfbefa6d785fc11eb8daa93ae5
Ashini L J <156600873+Ashinilj@users.noreply.github.com>
2026-02-12 23:39:01 +0530

    Time: 36 ms (59.17%) | Memory: 72.1 MB (70.53%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    let n= nums.length;
+    let ans= [];
+    nums.sort((a,b) => a-b)
+    for(let i=0; i<n-2; i++){
+        if(i > 0 && nums[i] === nums[i-1]) continue;
+
+        let left=i+1, right= n-1;
+        while(left < right){
+            let sum= nums[left] + nums[right] + nums[i];
+
+            if(sum === 0){
+                ans.push([nums[i], nums[left], nums[right]]);
+
+                while(left<right && nums[left] === nums[left+1]) left++;
+                while(left<right && nums[right] === nums[right-1]) right--;
+
+                left++;
+                right--;
+            }
+            else if(sum < 0) left++;
+            else right--;
+        }
+
+    }
+    return ans;
+};
