b0ac4b5755c3d97a317064c5d941880970c66acf
Ashini L J <156600873+Ashinilj@users.noreply.github.com>
2026-02-13 00:26:51 +0530

    Time: 5 ms (78.35%) | Memory: 57.4 MB (75.16%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let freq= new Map();
+    let left=0, right=0, maxLen=0;
+
+    while(right < s.length){
+        freq.set(s[right], (freq.get(s[right]) || 0) + 1);
+
+        while(freq.get(s[right]) > 1){
+            freq.set(s[left], freq.get(s[left]) - 1);
+            left++;
+        }
+        maxLen= Math.max(maxLen, right-left+1);
+        right++;
+    }
+    return maxLen;
+};
