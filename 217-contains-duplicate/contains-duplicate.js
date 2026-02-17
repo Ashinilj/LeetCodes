241b52f4899c931112595f4dcd235e5c01f9d640
Ashini L J <156600873+Ashinilj@users.noreply.github.com>
2026-02-17 23:01:49 +0530

    Time: 17 ms (67.01%) | Memory: 74.8 MB (32.92%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let set= new Set();
+    for(let val of nums){
+        if(set.has(val)){
+            return true;
+        }
+        set.add(val);
+    }
+    return false;
+};
