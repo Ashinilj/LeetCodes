d58e424a589a73de41285dcd8b033708c7a73c41
Ashini L J <156600873+Ashinilj@users.noreply.github.com>
2026-04-06 15:48:14 +0530

    Time: 19 ms (32.76%) | Memory: 60.7 MB (15.69%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,21 +4,19 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false;
-    let freq= new Map();
+    if(s.length !== t.length) return false;
 
+    let freq = new Map();
     for(let ch of s){
         freq.set(ch, (freq.get(ch) || 0) + 1);
     }
 
     for(let ch of t){
-        //if character in t is not present in s
         if(!freq.has(ch)) return false;
-        
-        //else decrease the freq of ch
-        freq.set(ch, freq.get(ch) - 1);
-        //if freq falls negative then t has more occuring of ch than s
+
+        freq.set(ch, freq.get(ch)-1);
         if(freq.get(ch) < 0) return false;
     }
+
     return true;
 };
