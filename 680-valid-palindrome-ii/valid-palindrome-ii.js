df72c6dc179c7fb728c7541db44e5305501a362a
Ashini L J <156600873+Ashinilj@users.noreply.github.com>
2026-04-08 15:40:00 +0530

    Time: 5 ms (87.08%) | Memory: 61 MB (67.69%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    const isPalindrome = (l, r) => {
+        while(l < r){
+            if(s[l] !== s[r]) return false;
+
+            l++;
+            r--;
+        }
+        return true;
+    };
+
+    let left = 0, right = s.length - 1;
+
+    while(left < right){
+        if(s[left] !== s[right]){
+            return isPalindrome(left + 1, right) || isPalindrome(left, right - 1);
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+};
